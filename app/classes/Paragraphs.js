51e6d81da2d903963eea7891138f3f911a94016d
Andrey Mitin <amitin@appdataroom.com>
2018-04-15 23:48:16 -0500

    When naming paragraphs, work with any subset of headings H1-6

--- app/classes/Paragraphs.js
+++ app/classes/Paragraphs.js
@@ -2,26 +2,36 @@ export default class Paragraphs {
   constructor(title, html, getHeading) {
     this.initializeParagraphs();
     this.initializeBreadcrumbs(title);
-    this.processHTML(html, getHeading);
+
+    if (typeof getHeading === "function") this.initializeGetHeading(getHeading);
+    else this.initializeGetHeading(() => null);
+
+    this.processHTML(html);
+  }
+  initializeGetHeading(getHeading) {
+    this.getHeading = getHeading;
   }
   initializeParagraphs() {
     this.paragraphs = [];
   }
   initializeBreadcrumbs(title) {
     this.breadcrumbs = [title];
+    this.bhMap = [0];
   }
   pushHeading(hIndex, heading) {
-    let bIndex = parseInt(hIndex) - 1;
     let headingPushed = false;
     let breadcrumbs = [];
-    this.breadcrumbs.forEach((crumb, idx) => {
-      if (idx < bIndex) return breadcrumbs.push(crumb);
-      if (idx === bIndex) {
+    this.bhMap.forEach((bhIndex, bIndex) => {
+      if (bhIndex < hIndex) return breadcrumbs.push(this.breadcrumbs[bIndex]);
+      if (bhIndex === hIndex) {
         headingPushed = true;
         return breadcrumbs.push(heading);
       }
     });
-    if ( ! headingPushed) breadcrumbs.push(heading);
+    if ( ! headingPushed) {
+      this.bhMap.push(hIndex);
+      breadcrumbs.push(heading);
+    }
     this.breadcrumbs = breadcrumbs;
   }
   pushParagraph(html) {
@@ -30,17 +40,25 @@ export default class Paragraphs {
     this.paragraphs.push({breadcrumbs: this.breadcrumbs, counter: this.counter, html});
     this.tempBreadcrumbs = this.breadcrumbs;
   }
-  processHTML(html, getHeading) {
-    $(html).find("p, h2, h3, h4, h5, h6").each((idx, el) => {
-      let matches;
-      let heading;
-      let tag = el.tagName;
-      let outerHTML = el.outerHTML;
-      if (tag === "P" && outerHTML) return this.pushParagraph(outerHTML);
-      if ((matches = /H([1-6])/.exec(tag)) !== null && matches[1] && (heading = getHeading(outerHTML))) {
-        this.pushHeading(matches[1], heading);
-      }
+  processHTML(html) {
+    $(html).find("p, h1, h2, h3, h4, h5, h6").each((idx, el) => {
+      this.processTypographicElement(el.tagName, el.outerHTML)
     });
+  }
+  processTypographicElement(tag, html) {
+    if (tag === "P") {
+      if (html) this.pushParagraph(html);
+      return;
+    }
+    this.processHeading(tag, html);
+  }
+  processHeading(tag, html) {
+    let heading = this.getHeading(html);
+    let matches = /H([1-6])/.exec(tag);
+    if (matches && matches[1] && heading) this.pushHeading(parseInt(matches[1]), heading);
+  }
+  getHeading(html) {
+    return this.getHeading(html);
   }
   getParagraphs() {
     return this.paragraphs;
